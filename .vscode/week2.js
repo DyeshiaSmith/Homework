65e7de2d7cca0f61276305da25555e3b7c39a986
D <dyeshiamonet@gmail.com>
2020-09-08 17:25:07 -0500

    finish HW Week2

---
+++ .vscode/week2.js
@@ -0,0 +1,13 @@
+let PizzaPlace = "dye slice";
+let numberOfToppings = 8;
+
+console.log(typeof PizzaPlace);
+console.log(typeof numberOfToppings);
+
+if(numberOfToppings < 10) {
+  console.log("Quality, not quanity")
+} else {
+  console.log("A Whole lot of pizza")
+}
+
+
