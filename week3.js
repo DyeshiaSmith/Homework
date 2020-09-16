7a12e98299739a7b1e1ed884e75b34c96cf98ebe
D <dyeshiamonet@gmail.com>
2020-09-16 18:05:11 -0500

    fix getPizza fxn and complete

--- week3.js
+++ week3.js
@@ -1,4 +1,4 @@
-const pizzaToppings = ["bacon", "sausage", "peperoni", "mushroom"];
+const pizzaToppings = ["bacon", "sausage", "peperoni", "mushrooms"];
 const crust = ["thin", "thick"];
 const size = ["small", "medium", "large"];
 
@@ -9,9 +9,18 @@ function greetCustomers (pizzaToppings) {
 console.log(greetCustomers(pizzaToppings));
 
 function getPizzaOrder (size, crust, ...pizzaToppings) {
-  return (`one ${size} ${crust}, with ${pizzaToppings} coming right up!`)
+  return (`one ${size} ${crust} crust pizza, with ${pizzaToppings} coming right up!`)
 }
 
-console.log(getPizzaOrder(size[0], crust[1], pizzaToppings))
+console.log(getPizzaOrder(size[0], crust[1], pizzaToppings[2], pizzaToppings[1]))
 
-function preparePizza() {}
+function preparePizza(size, crust, pizzaToppings) {
+  return (`one ${size} ${crust} crust pizza, with ${pizzaToppings} is being prepared`)
+}
+console.log(preparePizza(size[0], crust[0], pizzaToppings[0]));
+
+// let pizza = getPizzaOrder;
+// function servePizza (pizza) {
+//   return(`${pizza} pizza is ready!`)
+// }
+// console.log(servePizza(pizza));
