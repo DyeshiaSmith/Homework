923387c023b85a58ff1a4ab54f717ae620db9683
D <dyeshiamonet@gmail.com>
2020-09-19 00:52:15 -0500

    add some changes to week 3 HW

--- week3.js
+++ week3.js
@@ -5,7 +5,13 @@ const size = ["small", "medium", "large"];
 function greetCustomers (pizzaToppings) {
   return (`Welcome to pizza House, our toppings are ${pizzaToppings}`);
 }
-
+function greetCustomers() {
+  let greeting = `welcome to the pizza house, our toppings are: `;
+  for (let topping of pizzaToppings) {
+    order =+ `${topping}, `;
+  }
+  console.log(`${order. Comping Up!}`)
+}
 console.log(greetCustomers(pizzaToppings));
 
 function getPizzaOrder (size, crust, ...pizzaToppings) {
@@ -14,13 +20,14 @@ function getPizzaOrder (size, crust, ...pizzaToppings) {
 
 console.log(getPizzaOrder(size[0], crust[1], pizzaToppings[2], pizzaToppings[1]))
 
-function preparePizza(size, crust, pizzaToppings) {
-  return (`one ${size} ${crust} crust pizza, with ${pizzaToppings} is being prepared`)
+function preparePizza([orderSize, orderCrust, orderToppings]) {
+  console.log("...your pizza is cooking...");
+  return {
+    size: orderSize,
+    crust: orderCrust,
+    toppings: orderToppings
+  };
 }
-console.log(preparePizza(size[0], crust[0], pizzaToppings[0]));
 
-// let pizza = getPizzaOrder;
-// function servePizza (pizza) {
-//   return(`${pizza} pizza is ready!`)
-// }
-// console.log(servePizza(pizza));
+let cookedPizza = preparePizza(customerOrder);
+console.log(cookedPizza);
