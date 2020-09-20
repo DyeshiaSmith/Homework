de7612626e83811636551bec75a5cae77e0b298b
D <dyeshiamonet@gmail.com>
2020-09-20 12:28:30 -0500

    fix css background styles and add DOM methods

---
+++ week4Homework/index.js
@@ -0,0 +1,2 @@
+document.querySelector("footer").innerHTML= `<p><a href="https://github.com/DyeshiaSmith">Check Out My GitHub!</a></p>
+<p><a href="https://www.linkedin.com/in/dyeshia-smith/">Connect W/ Me on Linkedin!</a></p>`;
